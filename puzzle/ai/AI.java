ffcaf385e91a7c6d5998c00942946506a30094f2
jgjadaoag <jjadaoag1@up.edu.ph>
2016-05-12 22:19:19 +0800

    Random AI Opponent

---
+++ puzzle/ai/AI.java
@@ -0,0 +1,25 @@
+package puzzle.ai;
+
+import java.awt.Point;
+import java.util.Random;
+import java.util.LinkedList;
+
+import puzzle.board.BoardController;
+
+public class AI {
+	public static void act(BoardController board) {
+		Point p = nextMove(board.getSymbols(), board.getAISymbol(), board.getHumanSymbol());
+		if (p != null) board.setTile(p);
+	}
+	public static Point nextMove(char[][] board, char me, char opponent) {
+		LinkedList<Point> freeTiles = new LinkedList<Point>();
+		for (int iii = 0; iii < board.length; iii++) {
+			for (int jjj = 0; jjj < board[iii].length; jjj++) {
+				if(board[iii][jjj] == ' ') {
+					freeTiles.add(new Point(jjj, iii));
+				}
+			}
+		}
+		return freeTiles.size() > 0? freeTiles.get((new Random()).nextInt(freeTiles.size())): null;
+	}
+}
